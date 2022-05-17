8ba152258ae9bcb6d908b0197a45f79f6ac81738
becode <patez.sebastien@gmail.com>
2022-05-17 11:08:43 +0200

    mandatory part done

--- script.js
+++ script.js
@@ -3,33 +3,67 @@ let submit = document.body.querySelector("#submit");
 let compareBtn = document.body.querySelector(".compareBtn")
 let clearPrevious = document.body.querySelector("#clear")
 let OpenWeatherappId = "15503d995eb403b985f0761f2345534a"
+let cityOne = document.body.querySelector("#city1")
+let cityTwo = document.body.querySelector("#city2")
 // let storedWeather = { ...localStorage }
 
 function bigErrorOmg()
 {
-    alert("not a valid city you bum")
+    alert("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 }
+
+// a function that smells like wet poop
+// function getLatLon(city)
+// {
+//     let latLon = [];
+//     fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid=15503d995eb403b985f0761f2345534a")
+//     .then(res => res.json())
+//     .then(obj =>{
+//         // console.log(obj)
+//         latLon.push(obj[0].lat);
+//         latLon.push(obj[0].lon);
+//         console.log(obj[0].lat)
+        
+//     })
+//     // console.log(latLon)
+//     return latLon;
+// }
 
 function displayNewWeather(e)
 {
     if(!cities.value)
-        return ;
-    if (e.code === "Enter" || e.pointerType === "mouse")
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cities.value + "&APPID=" + OpenWeatherappId)
+    return;
+    let latLon = [];
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cities.value+"&limit=1&appid=15503d995eb403b985f0761f2345534a")
     .then(res => res.json())
     .then(obj =>{
-        if(obj.cod === '404')
-        {
-            bigErrorOmg();
-            return;
-        }
-        obj.main.temp = Math.ceil(Number(obj.main.temp) - 273.15);
-        let div = document.createElement("div");
-        
-        document.body.querySelector("main").appendChild(div);
-        div.innerHTML = obj.name + " " + obj.sys.country + " " + obj.main.temp + "°C";
-        window.localStorage.setItem(cities.value, JSON.stringify(obj))
+        // console.log(obj)
+        latLon.push(obj[0].lat);
+        latLon.push(obj[0].lon);
+        console.log(obj[0].lat)
+        fetch("http://api.openweathermap.org/data/2.5/forecast?lat="+latLon[0]+"&lon="+latLon[1]+"&appid=" + OpenWeatherappId)
+        .then(res => res.json())
+        .then(obj =>{
+            console.log(obj);
+            if(obj.cod === '404')
+            {
+                bigErrorOmg();
+                return;
+            }
+            obj.list[0].main.temp = Math.ceil(Number(obj.list[0].main.temp) - 273.15);
+            obj.list[1].main.temp = Math.ceil(Number(obj.list[1].main.temp) - 273.15);
+            obj.list[2].main.temp = Math.ceil(Number(obj.list[2].main.temp) - 273.15);
+            obj.list[3].main.temp = Math.ceil(Number(obj.list[3].main.temp) - 273.15);
+            obj.list[4].main.temp = Math.ceil(Number(obj.list[4].main.temp) - 273.15);
+            let div = document.createElement("div");
+            
+            document.body.querySelector("main").appendChild(div);
+            div.innerHTML = obj.city.name + " " + obj.list[0].main.temp + "°C" + " "+ obj.list[1].main.temp + "°C" + " "+ obj.list[2].main.temp + "°C" + " "+ obj.list[3].main.temp + "°C" + " "+ obj.list[4].main.temp + "°C" + " ";
+            window.localStorage.setItem(cities.value, JSON.stringify(obj))
+        })
     })
+    // console.log(latLon[1]);
+    // fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cities.value + "&APPID=" + OpenWeatherappId)
     .catch(error => console.log(error))
 }
 
@@ -56,14 +90,17 @@ function displayStoredWeather()
         document.body.querySelector("main").appendChild(div);
         div.setAttribute("class", "stored");
         let jsoned = JSON.parse(place)
-        div.innerHTML = jsoned.name + " " + jsoned.sys.country + " " + jsoned.main.temp + "°C";
+        div.innerHTML = jsoned.city.name + " " + jsoned.list[0].main.temp + "°C"+ " " + jsoned.list[1].main.temp + "°C"+ " " + jsoned.list[2].main.temp + "°C"+ " " + jsoned.list[3].main.temp + "°C"+ " " + jsoned.list[4].main.temp + "°C";
     }
 }
 
+
 displayStoredWeather()
 compareBtn.addEventListener("click", compareWeather);
-cities.addEventListener("keyup", displayNewWeather);
 submit.addEventListener("click", displayNewWeather);
+cities.addEventListener("keyup", (e)=>{
+    if(e.code === "Enter")
+        displayNewWeather(e)});
 clearPrevious.addEventListener("click", () => {
     window.localStorage.clear()
     for (let elem of document.body.querySelectorAll(".stored"))
